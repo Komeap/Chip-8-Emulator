6ef55c045af7076bbaa5149b307b707741e67096
Komeap <komeap.hynagame@gmail.com>
2026-01-13 09:53:31 +0100

    Modif README.md

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 #Projet CHIP-8 Emulator
 
-Bienvenue dans la documentation de mon projet émulateur chip-8.
-Hello test git-lab
+Bienvenue sur la documentation de mon émulateur CHIP-8, développé en C avec la bibliothèque SDL2.
 
-## Compilation
+### Installation des dépendances
+
+Avant de compiler, assurez-vous d'avoir les bibliothèques de développement SDL2 installées (notamment SDL_ttf pour la gestion du texte).
+Sous Linux : 
+        sudo apt-get install libsdl2-ttf-dev
+
+## Lancement du Projet
 
 Pour compiler ce projet on utilise un MakeFile, pour l'uitliser ouvrez un terminal à la racine et lancez :
 
@@ -11,87 +16,28 @@ Pour compiler ce projet on utilise un MakeFile, pour l'uitliser ouvrez un termin
  - make (créer l'éxécutable)
  - ./chip8 (nom de l'éxécutable)
 
- Si on rajoute des fichier librairies, il faut modifier la ligne du Makefile LIBS = ... -lMalibrairie (sans le lib au debut libmain -> -lmain)
+ - make clean && make && ./chip8
 
- Si on veut changer le fichier à éxécuter de maniére permanante, il faut modifier SRC = ...
+## Page D'acceuil Menu / Utilisation
 
- Si on veut compiler plusiuer fichier .c ensemble SRC = fichier1.c fichier2.c ..., ou SRC = $(wildcard *.c) éxécute tous les .c disponible
+Le Menu permet de choisir la ROM a charger en mémoire. 
+Utiliser les fléches HAUTS/BAS pour selectionerla ROM, 
+Et utiliser la touche entrée Pour valider et lancer la ROM
+La touche echap permet de quitter le ROM et le Menu d'acceuil (Ce qui ferme propremement le projet)
 
+## Objectif de réalisation atteint
 
-##  Doxygen (Cheat Sheet)
+        - logo : OK
+        - IBM : OK
+        - Corax : OK
+        - Flags : OK
+        - Quirks : OK
+        - Keypad : OK
+        - Beep : OK
 
-Pour voir la doc, lancer dans un navigateur docs/html/index.html (Si, vous lisez ça normalement c'est OK)
+        - Différents jeux trouver sur internet : OK
 
-Déja configurer, lorsque vous modifiez entrer dans le terminal '''doxygen Doxyfile'''
+        - Bonus : Page de Menu C, lancement et séléction de la ROM
 
-Voici la liste des balises (`@`) à utiliser pour documenter le projet efficacement.
-
-### 1. Les indispensables (Fonctions & Méthodes)
-* **`@brief`** : Description courte (une phrase) qui apparaît dans les listes sommaires.
-* **`@details`** : (Optionnel) Description longue. Tout texte mis après une ligne vide sous le `@brief` est considéré comme du détail.
-* **`@file`** : À mettre tout en haut d'un fichier pour le décrire.
-* **`@param [nom]`** : Décrit un paramètre. Précisez les unités si nécessaire (ex: ms, pixels).
-* **`@return`** : Décrit ce que renvoie la fonction.
-* **`@retval [valeur]`** : Pour détailler une valeur de retour spécifique (ex: `@retval -1 Erreur mémoire`).
-
-### 2. Alertes & Remarques (Très utile pour le Chip-8)
-* **`@note`** : Ajoute un encadré "Note" pour une info complémentaire.
-* **`@warning`** : Ajoute un encadré rouge "Avertissement" (ex: "Ne pas appeler avant init()").
-* **`@attention`** : Similaire à warning, pour attirer l'attention sur un point critique.
-
-### 3. Suivi de projet & Méta-données
-* **`@todo`** : Signale une fonctionnalité manquante ou à améliorer. Doxygen crée une page récapitulative de tous les TODOs.
-* **`@bug`** : Signale un bug connu qui n'est pas encore corrigé.
-* **`@deprecated`** : Indique qu'une fonction est obsolète et ne devrait plus être utilisée (utile si tu refais ton architecture).
-
-### 4. Exemples de code
-Pour montrer comment utiliser une fonction directement dans la doc :
-* **`@code` ... `@endcode`** : Permet d'insérer un bloc de code coloré.
-
-## Utilisation de Git(hub) 
-
-### Commande de base (dans l'odre)
-* git pull :  !! A faire toujours avant de commencer à travailler !! récupére les modifications de l'autre
-* Tu codes ...
-* Verifier que ca marche toujours make, ./chip8
-* git add .
-* git commit -m "Information sur ce que tu a fait " (Important ++ pour savoir ce que l'autre a fait et pour come back)
-* git push envoie les modifs
-
-### Commande complementaire
-
-* git status : affiche le status, 
-
-        Rouge :Fichier modifié mais pas pris en compte (faire git add)
-
-        Fichier prêt à être validé (faire git commit)
-
-* git log  : affiche le graph des commit 
-
-        --oneline : les recents sur une ligne
-
-        --all : tous
-
-        --graph : fait un graph
-
-* git restore . : efface tous depuis le dernier commit
-
-### Branche
-
-Pour travailler sur deux truc diff en même temps sans se gener
-* git checkout -b nom-de-ma-branche : Crée la branche et te met dessus direct
-* git branch : te dit sur qu'elle branche tu est (branche1 branch2 *BrancheActuelle)
-
-### Merge des branches après un travaille en paralléle
-
-* git .add + git commit !! Pas oublier d'enregistrer (localement) les modif de l'autre branche avant de changer de branche !!
-* git checkout branche1 : revenir sur la branche branche1
-
-         !! toujours commit + add . avant, pour garder les modification sur l'autre branche !!
-
-* git pull : Au cas où il y a eut des modif sur la main
-* git merge branche2 : fusion branche2 avec la branche où l'on est
-* git push : envoie le tous
-* git branch -d branch2 : supprimer branch2 une fois que tous est ok (Pas obligatoire)
 
 
--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_audio.h>
-#include <SDL2/SDL_ttf.h> // Pour la police d'éeciture, install linux : sudo apt-get install libsdl2-ttf-dev
+#include <SDL2/SDL_ttf.h> /// Pour la police d'éeciture, install linux : sudo apt-get install libsdl2-ttf-dev
 #include <time.h>
 
 #include "../include/display/display.h"
@@ -13,16 +13,27 @@
 #include "../include/processor.h"
 #include "../include/keyboard/keyboard.h"
 
-
+/// @brief Liste des liens des ROMS/jeux chip8
 const char *ROM_LIST[] = {"chip8_load/1-chip8-logo.ch8", "chip8_load/2-ibm-logo.ch8", "chip8_load/3-corax+.ch8", "chip8_load/4-flags.ch8", "chip8_load/5-quirks.ch8",
                            "chip8_load/6-keypad.ch8", "chip8_load/7-beep.ch8", "chip8_load/BLINKY", "chip8_load/TETRIS", "chip8_load/PONG2", "chip8_load/TICTAC",
                         "chip8_load/BLITZ", "chip8_load/BRIX", "chip8_load/CONNECT4", "chip8_load/GUESS", "chip8_load/HIDDEN", "chip8_load/INVADERS", "chip8_load/KALEID",
                         "chip8_load/MAZE", "chip8_load/MERLIN", "chip8_load/MISSILE", "chip8_load/PUZZLE", "chip8_load/SYZYGY", "chip8_load/TANK", "chip8_load/UFO", "chip8_load/VBRIX",
                         "chip8_load/VERS", "chip8_load/WIPEOFF"};
 
+/// @brief  nombre de Rom dans la ROM_LIST[]
 const int ROM_COUNT = sizeof(ROM_LIST) / sizeof(ROM_LIST[0]);
 
 void draw_text(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, int y, SDL_Color color) {
+    /**
+     * @brief Fonction qui permet d'afficher du texte a l'écran avec SDL
+     * @param renderer L'endroit ou ca affiche
+     * @param font police décriture
+     * @param text text a écrire
+     * @param x position x
+     * @param y position y
+     * @param color Couleur du texte
+     */
+    
     SDL_Surface *surface = TTF_RenderText_Solid(font, text, color);
     if (!surface) return;
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -51,46 +62,50 @@ SDL_Keycode keymap[16] = {
     SDLK_v  // F
 };
 
+/// @brief Gere le menu d'acceuil pour lancer les rom
 int run_menu() {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0){
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0){ /// check init
         return -1;
     }
     TTF_Init();
 
+    /// Créer la page d'acceuil (MENU), et le renderer(calque)
     SDL_Window *win = SDL_CreateWindow("CHIP-8 MENU", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
     SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
     
+    TTF_Font *font = TTF_OpenFont("Limited.ttf", 46); /// init de la police d ecriture, (Fichier ttf a la racine du projet)
 
-    TTF_Font *font = TTF_OpenFont("Limited.ttf", 46);
-
+    /// Variables pour la gestion du menu
     int selection = 0;
     bool in_menu = true;
     int result = -1;
     SDL_Event event;
 
+    /// gestion du nombre d'item par colonne pour que ca reste dans la page
     int items_per_col = (ROM_COUNT + 1) / 2;
 
+    /// boucle de gestion du Menu
     while (in_menu) {
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if (event.type == SDL_QUIT) { // SI on fait echap ou la croix
                 result = -1;
                 in_menu = false;
             }
-            if (event.type == SDL_KEYDOWN) {
+            if (event.type == SDL_KEYDOWN) { // Si on clique sur une touche
                 switch (event.key.keysym.sym) {
                     case SDLK_ESCAPE: 
                         result = -1;
                         in_menu = false;
                         break;
 
-                    case SDLK_UP:
+                    case SDLK_UP: // Si c'est la fléche up -> On change la selection
                         selection--;
-                        if (selection < 0){
+                        if (selection < 0){ 
                             selection = ROM_COUNT - 1;
                         }
                         break;
 
-                    case SDLK_DOWN:
+                    case SDLK_DOWN:  // Si c'est la fléche down -> On change la selection
                         selection++;
                         if (selection >= ROM_COUNT){
                             selection = 0;
@@ -98,7 +113,7 @@ int run_menu() {
                         break;
 
                     case SDLK_RETURN:
-                    case SDLK_KP_ENTER:
+                    case SDLK_KP_ENTER:  // Si c'est la enter -> On stop la boucle -> charge la rom
                         result = selection;
                         in_menu = false;
                         break;
@@ -106,38 +121,50 @@ int run_menu() {
             }
         }
 
-        // Affichage du Menu
+        /// Affichage du Menu
         SDL_SetRenderDrawColor(ren, 30, 30, 30, 255);
         SDL_RenderClear(ren);
 
         SDL_Color colTitre = {255, 200, 0, 255};
-        draw_text(ren, font, "CHIP-8 EMULATOR", 200, 20, colTitre);
+        draw_text(ren, font, "CHIP-8 EMULATOR", 200, 20, colTitre); /// ecriture
 
+        /// Boucle qui affiche les séléctions des ROMS
         for (int i = 0; i < ROM_COUNT; i++) {
             SDL_Color colText = {255, 255, 255, 255};
             char buffer[128];
             
             if (i == selection) {
                 colText.g = 0;
-                colText.b = 0; // Rchange la couleur pour celui select
-                sprintf(buffer, "> %s", ROM_LIST[i]);
+                colText.b = 0; // change la couleur pour celui select
+                sprintf(buffer, "> %s", ROM_LIST[i]); // Affiche une petit fléche ">" devant celui qui est select
             } else {
                 sprintf(buffer, "  %s", ROM_LIST[i]);
             }
 
-            int col = (i < items_per_col) ? 0 : 1;
-            int row = (col == 0) ? i : (i - items_per_col);
-            
+            int col, row;
+
+            // Si on n'a pas encore commencer la deuxieme col
+            if (i < items_per_col) {
+                col = 0;
+                row = i;
+            } 
+            else {
+                col = 1;
+                row = i - items_per_col;
+            }
+
+            /// Calcul des postion
             int x_pos = 20 + (col * 320);
             int y_pos = 60 + (row * 28);
 
-            draw_text(ren, font, buffer, x_pos, y_pos, colText);
+            draw_text(ren, font, buffer, x_pos, y_pos, colText); /// Affichage
         }
 
         SDL_RenderPresent(ren);
         SDL_Delay(16);
     }
 
+    /// Fermeture propre
     TTF_CloseFont(font);
     TTF_Quit();
     SDL_DestroyRenderer(ren);
@@ -146,61 +173,68 @@ int run_menu() {
     return result;
 }
 
-// Fonction qui lance une Rom
+/// @brief Fonction qui lance une ROM
+/// @param rom_path chemir d'accés de la rom (return de run_menu)
+/// @param monDisplay DIsplay
+/// @param MyKeyboard Keyboard
+/// @param MySpeaker Speaker
 bool run_game(const char *rom_path, struct Display *monDisplay, struct Keyboard *MyKeyboard, struct Speaker *MySpeaker) {
-    struct RAM *maMemoire = init_RAM();
-    if (load_RAM(maMemoire, rom_path) == 0) {
+    struct RAM *maMemoire = init_RAM(); /// init ram
+    if (load_RAM(maMemoire, rom_path) == 0) { // Charge la memoire + verification
         fprintf(stderr, "Erreur de  chargement ROM\n");
         free_RAM(maMemoire); 
         return true;
     }
 
 
-    if (Display_init(monDisplay, 10) != 0) {
+    if (Display_init(monDisplay, 10) != 0) { /// Init display + check
         free_RAM(maMemoire);
         return false;
     }
     
-    Display_set_colors(monDisplay, 200, 50, 155, 50, 50, 50); 
+    Display_set_colors(monDisplay, 200, 50, 155, 50, 50, 50); /// Setting couleur
 
-    struct CPU *monCPU = init_CPU(maMemoire, monDisplay, MyKeyboard, MySpeaker);
+    struct CPU *monCPU = init_CPU(maMemoire, monDisplay, MyKeyboard, MySpeaker); /// Init du CPU/processeur
     
     bool running = true;
+    /// Variable de gestion du temps, et des frames
     SDL_Event event;
     Uint32 last_tick = SDL_GetTicks();
     double delta_accumulator = 0;
-    
     int instructions_par_frame = 20;
     
     while (running) {
+        /// Gestion des ticks, pour le timer, frames et halting
         Uint32 current_tick = SDL_GetTicks();
         double dt = (current_tick - last_tick);
         last_tick = current_tick;
         delta_accumulator += dt;
 
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if (event.type == SDL_QUIT) { /// fermeture propre au cas ou
                 free_CPU(monCPU);
                 free_RAM(maMemoire);
                 Display_destroy(monDisplay);
                 return false;
             }
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+            if (event.key.keysym.sym == SDLK_ESCAPE) { /// Pour quitter le jeu avec echap
                 running = false;
             }
         }
 
+        /// Gestion des frame et de la vitesse de jeu
         while (delta_accumulator >= 15) {
             delta_accumulator -= 15;
             for (int i = 0; i < instructions_par_frame; i++) {
-                cpu_cycle(monCPU);
+                cpu_cycle(monCPU); /// Décodage de opcode
             }
-            cpu_update_timers(monCPU);
+            cpu_update_timers(monCPU); /// Fonction qui gére le temps
         }
-        Display_update(monDisplay);
+        Display_update(monDisplay);/// Affichage
         SDL_Delay(1);
     }
 
+    /// Fermeture propre
     Display_destroy(monDisplay);
     free_CPU(monCPU);
     free_RAM(maMemoire);
@@ -210,7 +244,7 @@ bool run_game(const char *rom_path, struct Display *monDisplay, struct Keyboard 
 
 int main() {
     srand(time(NULL));
-
+    /// init
     struct Display *monDisplay = (struct Display*)calloc(1, sizeof(struct Display));
     struct Keyboard *MyKeyboard = (struct Keyboard*)calloc(1, sizeof(struct Keyboard));
     struct Speaker *MySpeaker = (struct Speaker*)calloc(1, sizeof(struct Speaker));
@@ -221,17 +255,18 @@ int main() {
     bool app_running = true;
 
     while (app_running) {
-        int choix = run_menu();
+        int choix = run_menu(); // LAnce le menu en premier
 
-        if (choix == -1) {
+        if (choix == -1) { // SI on a un choix de rom dans le menu
             app_running = false;
         } else {
-            // Lance un jeu, creer ca propre fenetre
+            // Lance une ROM
             bool back = run_game(ROM_LIST[choix], monDisplay, MyKeyboard, MySpeaker);
             if (!back) app_running = false;
         }
     }
 
+    // Fermeture Propre
     free(monDisplay);
     free(MyKeyboard);
     free(MySpeaker);
@@ -239,90 +274,3 @@ int main() {
 
     return 0;
 }
-
-/*
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <fichier_rom>\n", argv[0]);
-        return 1;
-    }
-    srand(time(NULL));
-    struct RAM *maMemoire = init_RAM();
-
-    //printf("%d\n", load_RAM(maMemoire, "/home/julien/Cours/Chip-8_Project/1-chip8-logo.ch8"));
-
-
-    if (load_RAM(maMemoire, argv[1]) == 0) {
-        fprintf(stderr, "Erreur chargement ROM.\n");
-        return 1;
-    }
-    
-    //print_RAM(maMemoire);
-
-    //printf("%s\n",instruction_as_str((uint16_t)0xFF));
-    //print_instruct(maMemoire);
-    struct Display *monDisplay = (struct Display*)calloc(1, sizeof(struct Display));
-    if (Display_init(monDisplay, 10) != 0) {
-        fprintf(stderr, "Erreur: Impossible d'initialiser l'affichage.\n");
-        return 1;
-    }
-
-    struct Keyboard *MyKeyboard = (struct Keyboard*)calloc(1, sizeof(struct Keyboard));
-    if (Keyboard_init(MyKeyboard) != 0) {
-        fprintf(stderr, "Erreur: Impossible d'initialiser l'affichage.\n");
-        return 1;
-    }
-
-    struct Speaker *MySpeaker = (struct Speaker*)calloc(1, sizeof(struct Speaker));
-    if (Speaker_init(MySpeaker) != 0) {
-        fprintf(stderr, "Erreur: Impossible d'initialiser l'affichage.\n");
-        return 1;
-    }
-
-    Display_set_colors(monDisplay, 155,155,155,155,52,230);
-
-
-    struct CPU *monCPU = init_CPU(maMemoire, monDisplay, MyKeyboard, MySpeaker);
-
-    printf("Démarrage de l'émulation...\n");
-    
-    bool running = true;
-    SDL_Event event;
-
-    Uint32 last_tick = SDL_GetTicks();
-    double delta_accumulator = 0;
-    
-    int instructions_par_frame = 10;
-    
-    while (running) {
-        Uint32 current_tick = SDL_GetTicks();
-        double dt = (current_tick - last_tick);
-        last_tick = current_tick;
-        delta_accumulator += dt;
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-        if (delta_accumulator >= 15) {
-            delta_accumulator -= 15;
-            for (int i = 0; i < instructions_par_frame; i++) {
-                cpu_cycle(monCPU);
-            }
-            cpu_update_timers(monCPU);
-            Display_update(monDisplay);
-        }
-
-        SDL_Delay(1);
-    }
-
-    printf("Fermeture de l'émulateur.\n");
-    Display_destroy(monDisplay);
-    Speaker_destroy(MySpeaker);
-    free(monDisplay);
-    free(MySpeaker);
-
-    return 0;
-}
-*/
